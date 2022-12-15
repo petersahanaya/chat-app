706505a0ad3aee6ab0a028e53b949497f135a163
petersahanaya <petersahanaya09@gmail.com>
2022-12-15 17:12:48 +0900

    initial commit

--- src/routes/api/upload/+server.ts
+++ src/routes/api/upload/+server.ts
@@ -1,19 +1,17 @@
 import type { RequestHandler } from "@sveltejs/kit";
 import { User } from "$lib/models/index";
 import JWT from 'jsonwebtoken'
-import { REFRESH_TOKEN,  ACCESS_TOKEN} from "$env/static/private";
 
 export const POST : RequestHandler = async ({request, cookies}) => {
     const body = await request.json();
-    console.log(body)
 
     if(body) {
            await User.update({image : body.url}, {where : { username : body.username }})
             const user : any = await User.findOne({where : {username : body.username}})
 
-           const accessToken = JWT.sign({username : user.username, id : user.id, email : user.email, image : user.image, userId : user.userId}, ACCESS_TOKEN, {expiresIn : '15m'})
+           const accessToken = JWT.sign({username : user.username, id : user.id, email : user.email, image : user.image, userId : user.userId}, import.meta.env.VITE_ACCESS_TOKEN, {expiresIn : '15m'})
 
-           const refreshToken = JWT.sign({username : user.username, id : user.id, email : user.email, image : user.image, userId : user.userId}, REFRESH_TOKEN, {expiresIn : '15m'})
+           const refreshToken = JWT.sign({username : user.username, id : user.id, email : user.email, image : user.image, userId : user.userId}, import.meta.env.VITE_REFRESH_TOKEN, {expiresIn : '15m'})
 
             cookies.set('token', accessToken, {path : "/", httpOnly : true, expires : new Date(Date.now() + 1000 * 60 * 15)})
 
